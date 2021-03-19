04feab94f2a53a5e544ecd24b11c48524f1afc1e
Ant. Patricia C. Sampaio <patricia_sampaio@virgo.atlantico.net.br>
2021-03-19 18:32:49 -0300

    Foi criado o metodo DELETE ao projeto

--- cadastroUsuario/pontoTel/usuarios/views.py
+++ cadastroUsuario/pontoTel/usuarios/views.py
@@ -1,16 +1,52 @@
 from django.http import request
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404 
 from .models import Person #importando o model person
 from .forms import PersonForm
 
+#get_obj...404 tenta acessar o objeto que o usuario esta acessando
 def persons_list(request):
     person =  Person.objects.all()
     return render(request, 'person.html', {'persons': person}) 
 
 def persons_new(request):
-    form =  PersonForm(request.POST, request.FILES, None)
+    form =  PersonForm(request.POST or None, request.FILES or None)
     #validar e salvar os dados
     if form.is_valid():
         form.save()
         return redirect('person_list')
     return render(request, 'person_forms.html',{'form': form})
+
+def persons_update(request, id):
+    person =  get_object_or_404(Person, pk=id)
+    form =  PersonForm(request.POST or None, request.FILES or None, instance=person)
+
+    if form.is_valid():
+        form.save()
+        return redirect('person_list')
+    return render(request, 'person_forms.html', {'form': form})
+
+def persons_delete(request, id):
+    person = get_object_or_404(Person, pk=id)
+    
+    if request.method == 'POST':
+        person.delete()
+        return redirect('person_list')
+
+    return render(request, 'person_delete_confirm.html', {'person': person})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
